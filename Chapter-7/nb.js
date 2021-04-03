e1e7b58e96b9d06a8a991d31d22095ae6fe95dc6
ji5485 <dong5485@gmail.com>
2021-04-03 13:55:54 +0000

    Apply Alternatives of Array

--- Chapter-7/nb.js
+++ Chapter-7/nb.js
@@ -20,9 +20,9 @@ bulletproof = ['d#m', 'g#', 'b', 'f#', 'g#m', 'c#']
 
 var songs = []
 var labels = []
-var allChords = []
-var labelCounts = []
-var labelProbabilities = []
+var allChords = new Set()
+var labelCounts = {}
+var labelProbabilities = {}
 var chordCountsInLabels = {}
 var probabilityOfChordsInLabels = {}
 
@@ -30,11 +30,7 @@ function train(chords, label) {
   songs.push([label, chords])
   labels.push(label)
 
-  chords.forEach(chord => {
-    if (!allChords.includes(chord)) {
-      allChords.push(chord)
-    }
-  })
+  chords.forEach(chord => allChords.add(chord))
 
   if (Object.keys(labelCounts).includes(label)) {
     labelCounts[label] = labelCounts[label] + 1
