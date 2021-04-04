f691123824fbb7b016fbe9a08b663a0f55d79061
ji5485 <dong5485@gmail.com>
2021-04-04 14:52:06 +0000

    Transfer Global Functions To Classifier Object

--- Chapter-7/nb_fp.js
+++ Chapter-7/nb_fp.js
@@ -1,34 +1,60 @@
 const classifier = {
-  allChords: new Set(),
+  songList: {
+    allChords: new Set(),
+    difficulties: ['easy', 'medium', 'hard'],
+    songs: [],
+    addSong(name, chords, difficulty) {
+      this.songs.push({ name, chords, difficulty: this.difficulties[difficulty] })
+    },
+  },
   labelCounts: new Map(),
   labelProbabilities: new Map(),
   chordCountsInLabels: new Map(),
   smoothing: 1.01,
-  chordCountForDifficulty: function (difficulty, testChord) {
-    return songList.songs.reduce(function (counter, song) {
+  trainAll() {
+    this.songList.songs.forEach(function (song) {
+      this.train(song.chords, song.difficulty)
+    }, this)
+
+    this.setLabelProbabilities()
+  },
+  train(chords, label) {
+    chords.forEach(chord => this.songList.allChords.add(chord))
+
+    if (Array.from(this.labelCounts.keys()).includes(label)) {
+      this.labelCounts.set(label, this.labelCounts.get(label) + 1)
+    } else {
+      this.labelCounts.set(label, 1)
+    }
+  },
+  setLabelProbabilities() {
+    this.labelCounts.forEach(function (_count, label) {
+      this.labelProbabilities.set(label, this.labelCounts.get(label) / this.songList.songs.length)
+    }, this)
+  },
+  chordCountForDifficulty(difficulty, testChord) {
+    return this.songList.songs.reduce(function (counter, song) {
       if (song.difficulty === difficulty) {
-        counter += song.chords.filter(function (chord) {
-          return chord === testChord
-        }).length
+        counter += song.chords.filter(chord => chord === testChord).length
       }
       return counter
     }, 0)
   },
-  setProbabilityOfChordsInLabels: function () {
+  setProbabilityOfChordsInLabels() {
     this.chordCountsInLabels.forEach(function (_chords, difficulty) {
       Object.keys(this.chordCountsInLabels.get(difficulty)).forEach(function (chord) {
-        this.chordCountsInLabels.get(difficulty)[chord] /= songList.songs.length
+        this.chordCountsInLabels.get(difficulty)[chord] /= classifier.songList.songs.length
       }, this)
     }, this)
   },
-  likelihoodFromChord: function (difficulty, chord) {
-    return this.chordCountForDifficulty(difficulty, chord) / songList.songs.length
+  likelihoodFromChord(difficulty, chord) {
+    return this.chordCountForDifficulty(difficulty, chord) / classifier.songList.songs.length
   },
-  valueForChordDifficulty: function (difficulty, chord) {
+  valueForChordDifficulty(difficulty, chord) {
     const value = this.likelihoodFromChord(difficulty, chord)
     return value ? value + this.smoothing : 1
   },
-  classify: function (chords) {
+  classify(chords) {
     return new Map(
       Array.from(this.labelProbabilities.entries()).map(labelWithProbability => {
         const difficulty = labelWithProbability[0]
@@ -44,61 +70,30 @@ const classifier = {
   },
 }
 
-const songList = {
-  difficulties: ['easy', 'medium', 'hard'],
-  songs: [],
-  addSong: function (name, chords, difficulty) {
-    this.songs.push({ name, chords, difficulty: this.difficulties[difficulty] })
-  },
-}
-
-function train(chords, label) {
-  chords.forEach(chord => classifier.allChords.add(chord))
-
-  if (Array.from(classifier.labelCounts.keys()).includes(label)) {
-    classifier.labelCounts.set(label, classifier.labelCounts.get(label) + 1)
-  } else {
-    classifier.labelCounts.set(label, 1)
-  }
-}
-
-function setLabelProbabilities() {
-  classifier.labelCounts.forEach(function (_count, label) {
-    classifier.labelProbabilities.set(
-      label,
-      classifier.labelCounts.get(label) / songList.songs.length,
-    )
-  })
-}
-
-function trainAll() {
-  songList.songs.forEach(function (song) {
-    train(song.chords, song.difficulty)
-  })
-
-  setLabelProbabilities()
-}
-
 // Test Code //
 
 const wish = require('wish')
 
 describe('the file', function () {
-  songList.addSong('imagine', ['c', 'cmaj7', 'f', 'am', 'dm', 'g', 'e7'], 0)
-  songList.addSong('somewhereOverTheRainbow', ['c', 'em', 'f', 'g', 'am'], 0)
-  songList.addSong('tooManyCooks', ['c', 'g', 'f'], 0)
-  songList.addSong('iWillFollowYouIntoTheDark', ['f', 'dm', 'bb', 'c', 'a', 'bbm'], 1)
-  songList.addSong('babyOneMoreTime', ['cm', 'g', 'bb', 'eb', 'fm', 'ab'], 1)
-  songList.addSong('creep', ['g', 'gsus4', 'b', 'bsus4', 'c', 'cmsus4', 'cm6'], 1)
-  songList.addSong(
+  classifier.songList.addSong('imagine', ['c', 'cmaj7', 'f', 'am', 'dm', 'g', 'e7'], 0)
+  classifier.songList.addSong('somewhereOverTheRainbow', ['c', 'em', 'f', 'g', 'am'], 0)
+  classifier.songList.addSong('tooManyCooks', ['c', 'g', 'f'], 0)
+  classifier.songList.addSong('iWillFollowYouIntoTheDark', ['f', 'dm', 'bb', 'c', 'a', 'bbm'], 1)
+  classifier.songList.addSong('babyOneMoreTime', ['cm', 'g', 'bb', 'eb', 'fm', 'ab'], 1)
+  classifier.songList.addSong('creep', ['g', 'gsus4', 'b', 'bsus4', 'c', 'cmsus4', 'cm6'], 1)
+  classifier.songList.addSong(
     'paperBag',
     ['bm7', 'e', 'c', 'g', 'b7', 'f', 'em', 'a', 'cmaj7', 'em7', 'a7', 'f7', 'b'],
     2,
   )
-  songList.addSong('toxic', ['cm', 'eb', 'g', 'cdim', 'eb7', 'd7', 'db7', 'ab', 'gmaj7', 'g7'], 2)
-  songList.addSong('bulletproof', ['d#m', 'g#', 'b', 'f#', 'g#m', 'c#'], 2)
+  classifier.songList.addSong(
+    'toxic',
+    ['cm', 'eb', 'g', 'cdim', 'eb7', 'd7', 'db7', 'ab', 'gmaj7', 'g7'],
+    2,
+  )
+  classifier.songList.addSong('bulletproof', ['d#m', 'g#', 'b', 'f#', 'g#m', 'c#'], 2)
 
-  trainAll()
+  classifier.trainAll()
 
   it('classifies', function () {
     const classified = classifier.classify(['f#m7', 'a', 'dadd9', 'dmaj7', 'bm', 'bm7', 'd', 'f#m'])
